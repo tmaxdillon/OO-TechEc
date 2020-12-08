f5b7a21f8d9d81e52bb9b9e50f3aade51120c54d
Trent Dillon <tmaxd@uw.edu>
2020-12-08 07:48:11 -0800

    regainted wec sim dir

---
+++ optimization_codeset/wave_codeset/wec_sim/unpackWS.m
@@ -0,0 +1,13 @@
+dc = 5;
+B = 6;
+
+struct6m(dc).H = H;
+struct6m(dc).T = T;
+struct6m(dc).mat = mat;
+struct6m(dc).B = B;
+struct6m(dc).P = P;
+
+struct6m_opt.H = struct6m(1).H;
+struct6m_opt.T = struct6m(1).T;
+struct6m_opt.B = struct6m(1).B;
+struct6m_opt.P = struct6m(1).P;
